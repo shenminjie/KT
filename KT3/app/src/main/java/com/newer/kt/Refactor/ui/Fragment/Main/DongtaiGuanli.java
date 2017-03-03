9a4a45bf5cb73e4cf5e11196c584aa4834120ca5
leo <1104133609@qq.com>
2017-03-03 16:04:16 +0800

    mend a little in ‘match’

--- KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/DongtaiGuanli.java
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/DongtaiGuanli.java
@@ -1,9 +1,11 @@
 package com.newer.kt.Refactor.ui.Fragment.Main;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.newer.kt.R;
@@ -11,6 +13,7 @@ import com.newer.kt.R;
 public class DongtaiGuanli extends AppCompatActivity {
 
     private TextView tv_zhong;
+    private ImageView image_vs_item_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +32,17 @@ public class DongtaiGuanli extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        image_vs_item_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private void initView() {
         tv_zhong = ((TextView) findViewById(R.id.tv_zhong));
+        image_vs_item_back = ((ImageView) findViewById(R.id.image_vs_item_back));
     }
 }
--- KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Saishi_Shangchuan.java
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Saishi_Shangchuan.java
@@ -2,14 +2,39 @@ package com.newer.kt.Refactor.ui.Fragment.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.newer.kt.R;
 
 public class Saishi_Shangchuan extends AppCompatActivity {
 
+    private ImageView image_vs_item_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saishi__shangchuan);
+
+        initView();
+        initOnclick();
+        initDate();
+    }
+
+
+    private void initDate() {
+
+    }
+    private void initOnclick() {
+        image_vs_item_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initView() {
+        image_vs_item_back = ((ImageView) findViewById(R.id.image_vs_item_back));
     }
 }
