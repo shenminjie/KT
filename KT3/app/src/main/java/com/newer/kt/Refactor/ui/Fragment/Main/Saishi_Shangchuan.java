9be688902ea2afecc9fed7b80a20b462c59fa258
leo <1104133609@qq.com>
2017-03-06 16:47:51 +0800

    xwz 3.6

--- KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Saishi_Shangchuan.java
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Saishi_Shangchuan.java
@@ -3,13 +3,18 @@ package com.newer.kt.Refactor.ui.Fragment.Main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.newer.kt.R;
 
 public class Saishi_Shangchuan extends AppCompatActivity {
 
     private ImageView image_vs_item_back;
+    private ListView lv_daishangchuan;
+    private BaseAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +22,48 @@ public class Saishi_Shangchuan extends AppCompatActivity {
         setContentView(R.layout.activity_saishi__shangchuan);
 
         initView();
-
+        initAdapter();
         //
         initOnclick();
 
+
         initDate();
+
     }
 
+    private void initAdapter() {
+        adapter=new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 2;
+            }
 
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                convertView=View.inflate(getApplicationContext(),R.layout.item_shangchuanlist,null);
+
+                return convertView;
+            }
+        };
+        lv_daishangchuan.setAdapter(adapter);
+
+    }
 
 
     private void initDate() {
 
     }
+
     private void initOnclick() {
         image_vs_item_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +75,6 @@ public class Saishi_Shangchuan extends AppCompatActivity {
 
     private void initView() {
         image_vs_item_back = ((ImageView) findViewById(R.id.image_vs_item_back));
+        lv_daishangchuan = ((ListView) findViewById(R.id.lv_daishangchuan));
     }
 }
