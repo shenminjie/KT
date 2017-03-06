f1414ac826246954dc74a46535ffd82b7467479d
jasonxw@163.com <xuwenzhe19960326>
2017-03-06 13:41:09 +0800

    add portrait 'create game'

--- KT3/app/src/main/java/com/newer/kt/Refactor/KTApplication.java
+++ KT3/app/src/main/java/com/newer/kt/Refactor/KTApplication.java
@@ -26,6 +26,11 @@ import com.newer.kt.Refactor.db.THDatabaseLoader;
 import com.newer.kt.Refactor.db.UsersDaoHelper;
 import com.newer.kt.Refactor.ui.Avtivity.CachedActivity;
 import com.newer.kt.entity.UserMsg;
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.umeng.um_share.App;
 import com.uuzuche.lib_zxing.activity.ZXingLibrary;
 import com.yolanda.nohttp.NoHttp;
@@ -77,7 +82,7 @@ public class KTApplication extends App {
         mLocationClient.registerLocationListener(myListener);    //注册监听函数
         initLocation();
         mLocationClient.start();
-
+initImageLoader();
 //        configuration = new YoukuPlayerBaseConfiguration(this) {
 //
 //
@@ -123,6 +128,51 @@ public class KTApplication extends App {
         initDB();
         initButTags();
         ZXingLibrary.initDisplayOpinion(this);
+
+    }
+
+    private void initImageLoader() {
+        if(!ImageLoader.getInstance().isInited()){
+            ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                    this)
+                    // max width, max height，即保存的每个缓存文件的最大长宽
+                    .memoryCacheExtraOptions(480, 800)
+//                    // Can slow ImageLoader, use it carefully (Better don't use it)设置缓存的详细信息，最好不要设置这个
+//                    .discCacheExtraOptions(480, 800, Bitmap.CompressFormat.JPEG, 75, null)
+                    // 线程池内加载的数量
+                    .threadPoolSize(3)
+                    // 线程优先级
+                    .threadPriority(Thread.NORM_PRIORITY - 2)
+            /*
+             * When you display an image in a small ImageView
+             *  and later you try to display this image (from identical URI) in a larger ImageView
+             *  so decoded image of bigger size will be cached in memory as a previous decoded image of smaller size.
+             *  So the default behavior is to allow to cache multiple sizes of one image in memory.
+             *  You can deny it by calling this method:
+             *  so when some image will be cached in memory then previous cached size of this image (if it exists)
+             *   will be removed from memory cache before.
+             */
+                    .denyCacheImageMultipleSizesInMemory()
+
+                    // You can pass your own memory cache implementation你可以通过自己的内存缓存实现
+                    // .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024))
+                    // .memoryCacheSize(2 * 1024 * 1024)
+                    //硬盘缓存50MB
+                    .diskCacheSize(50 * 1024 * 1024)
+                    //将保存的时候的URI名称用MD5
+                    .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                    // 加密
+                    .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())//将保存的时候的URI名称用HASHCODE加密
+                    .tasksProcessingOrder(QueueProcessingType.LIFO)
+                    .diskCacheFileCount(100) //缓存的File数量
+                    // .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+                    // .imageDownloader(new BaseImageDownloader(context, 5 * 1000,
+                    // 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+                    .writeDebugLogs() // Remove for release app
+                    .build();
+            // Initialize ImageLoader with configuration.
+            ImageLoader.getInstance().init(config);
+        }
 
     }
 
--- KT3/app/src/main/java/com/newer/kt/ktmatch/TeamAdapter.java
+++ KT3/app/src/main/java/com/newer/kt/ktmatch/TeamAdapter.java
@@ -59,47 +59,6 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.MyViewHolder> 
         holder.mSexImg.setImageResource(valueList.get(position).get("gender").equals("MM")?R.mipmap.nv:R.mipmap.nan);
 //        holder.mIcon.setImageResource(valueList.get(position).get("gender").equals("MM")?R.mipmap.nv:R.mipmap.nan);
 
-        if(!ImageLoader.getInstance().isInited()){
-            ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-                    mContext)
-                    // max width, max height，即保存的每个缓存文件的最大长宽
-                    .memoryCacheExtraOptions(480, 800)
-//                    // Can slow ImageLoader, use it carefully (Better don't use it)设置缓存的详细信息，最好不要设置这个
-//                    .discCacheExtraOptions(480, 800, Bitmap.CompressFormat.JPEG, 75, null)
-                    // 线程池内加载的数量
-                    .threadPoolSize(3)
-                    // 线程优先级
-                    .threadPriority(Thread.NORM_PRIORITY - 2)
-            /*
-             * When you display an image in a small ImageView
-             *  and later you try to display this image (from identical URI) in a larger ImageView
-             *  so decoded image of bigger size will be cached in memory as a previous decoded image of smaller size.
-             *  So the default behavior is to allow to cache multiple sizes of one image in memory.
-             *  You can deny it by calling this method:
-             *  so when some image will be cached in memory then previous cached size of this image (if it exists)
-             *   will be removed from memory cache before.
-             */
-                    .denyCacheImageMultipleSizesInMemory()
-
-                    // You can pass your own memory cache implementation你可以通过自己的内存缓存实现
-                    // .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024))
-                    // .memoryCacheSize(2 * 1024 * 1024)
-                    //硬盘缓存50MB
-                    .diskCacheSize(50 * 1024 * 1024)
-                    //将保存的时候的URI名称用MD5
-                    .diskCacheFileNameGenerator(new Md5FileNameGenerator())
-                    // 加密
-                    .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())//将保存的时候的URI名称用HASHCODE加密
-                    .tasksProcessingOrder(QueueProcessingType.LIFO)
-                    .diskCacheFileCount(100) //缓存的File数量
-                    // .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
-                    // .imageDownloader(new BaseImageDownloader(context, 5 * 1000,
-                    // 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
-                    .writeDebugLogs() // Remove for release app
-                    .build();
-            // Initialize ImageLoader with configuration.
-            ImageLoader.getInstance().init(config);
-        }
         ImageLoader.getInstance().displayImage(valueList.get(position).get("avatar").toString(),holder.mIcon,getListOptions());
     }
     public static DisplayImageOptions getListOptions() {
