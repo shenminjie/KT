77e13c401f2788931ff8eb59892c37af1477e76b
leo <1104133609@qq.com>
2017-03-07 13:21:26 +0800

    xwz3.7.5

--- KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Class_StuManager.java
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Class_StuManager.java
@@ -1,29 +1,51 @@
 package com.newer.kt.Refactor.ui.Fragment.Main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.newer.kt.R;
+import com.newer.kt.entity.Student;
 
 public class Class_StuManager extends AppCompatActivity {
 
     private ListView lv_class_stuManager;
     private BaseAdapter adapter;
+    private ImageView image_vs_item_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_stu_manager);
         initView();
-        initOnclick();
+
         initDate();
         initAdapter();
+        initOnclick();
     }
 
+    private void initOnclick() {
+        lv_class_stuManager.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getApplicationContext(), Student_Info.class);
+                startActivity(intent);
+            }
+        });
+
+        image_vs_item_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 
     private void initAdapter() {
         adapter=new BaseAdapter() {
@@ -54,10 +76,11 @@ public class Class_StuManager extends AppCompatActivity {
     private void initDate() {
     }
 
-    private void initOnclick() {
-    }
 
     private void initView() {
         lv_class_stuManager = ((ListView) findViewById(R.id.lv_class_stuManager));
+        image_vs_item_back = ((ImageView) findViewById(R.id.image_vs_item_back));
+
+
     }
 }
--- KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Student_Info.java
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/Student_Info.java
@@ -2,14 +2,34 @@ package com.newer.kt.Refactor.ui.Fragment.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.frame.app.utils.ImageViewUtils;
 import com.newer.kt.R;
 
 public class Student_Info extends AppCompatActivity {
+
+    private ImageView image_vs_item_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student__info);
+        initView();
+        initOnclick();
+    }
+
+    private void initOnclick() {
+        image_vs_item_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initView() {
+        image_vs_item_back = ((ImageView) findViewById(R.id.image_vs_item_back));
     }
 }
