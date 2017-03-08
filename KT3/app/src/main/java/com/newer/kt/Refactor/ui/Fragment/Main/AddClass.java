b9a66d3b2aa038a4c519e13e971302e01a023e1f
leo <1104133609@qq.com>
2017-03-08 09:07:37 +0800

    xwz3.8.1

---
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/AddClass.java
@@ -0,0 +1,15 @@
+package com.newer.kt.Refactor.ui.Fragment.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.newer.kt.R;
+
+public class AddClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class2);
+    }
+}
--- KT3/app/src/main/java/com/newer/kt/fragment/peixun/JiNengFragment_List.java
+++ KT3/app/src/main/java/com/newer/kt/fragment/peixun/JiNengFragment_List.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class JiNengFragment_List extends AppCompatActivity {
+public class   JiNengFragment_List extends AppCompatActivity {
 
     private ListView lv_jinengList;
     private BaseAdapter adapter;
