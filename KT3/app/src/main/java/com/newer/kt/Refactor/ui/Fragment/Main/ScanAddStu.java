41b192c57c8358c66e04935fb9c34f48ecb65636
jasonxw@163.com <xuwenzhe19960326>
2017-03-09 15:57:18 +0800

    add cam_add_stu

---
+++ KT3/app/src/main/java/com/newer/kt/Refactor/ui/Fragment/Main/ScanAddStu.java
@@ -0,0 +1,26 @@
+package com.newer.kt.Refactor.ui.Fragment.Main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import shengchengerweima.CamScanActivity;
+
+/**
+ * Created by litli on 2017/3/9.
+ */
+
+public class ScanAddStu extends CamScanActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void recvCode(String result) {
+        super.recvCode(result);
+        startActivity(new Intent(getBaseContext(),Student_Info.class).putExtra("code",result));
+    }
+}
